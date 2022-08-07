d943ef7e446729b3bfe026198582c4e0b887dabf
Alphonse <apuertaj2@gmail.com>
2022-08-06 23:57:42 -0500

    Generating token and sending it in cookie

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,3 +1,33 @@
+import * as jwt from 'jsonwebtoken'
+import { serialize } from 'cookie'
+
 export default function loginHandler(req, res) {
-  return res.json('login route')
+  const { email, password } = req.body
+
+  // check if email and password are valid
+
+  // if email exists
+
+  // if password is correct
+
+  if (email === 'admin@local.local' && password === 'admin') {
+    const token = jwt.sign({
+      exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 30),
+      email: 'admin@local.local',
+      username: 'master'
+    }, 'secret')
+
+    const serialized = serialize('myTokenName', token, {
+      httpOnly: true,
+      segure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      path: '/'
+    })
+
+    res.setHeader('Set-Cookie', serialized)
+    return res.json('login successfully')
+  }
+
+  return res.status(401).json({ error: 'Invalid credentials' })
 }
